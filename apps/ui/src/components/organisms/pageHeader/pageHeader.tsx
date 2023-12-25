8baed40642f299f56c4ed4de155ce5043b1ff731
Vitali Karpuk <vitalikkarpuk652@gmail.com>
2023-12-25 14:29:22 +0300

    added balance on header page

--- apps/ui/src/components/organisms/pageHeader/pageHeader.tsx
+++ apps/ui/src/components/organisms/pageHeader/pageHeader.tsx
@@ -1,16 +1,28 @@
 import { useState } from 'react';
+import Image from 'next/image';
+import classNames from 'classnames';
 import { Button } from '../../atoms/button';
 import { Variant } from '../../atoms/button/types';
 import { ConnectWalletButton } from '../../molecules/connectWalletButton';
 import StakeModalController from '../../molecules/stakeModal/StakeModalController';
+import useAuroWalletCore from '../../../hooks/useAuroWalletCore';
+import useAddressBalance from '../../../hooks/useAddressBalance';
+import DollarIcon from './Dollar.svg';
+
 import style from './index.module.css';
 
 const PageHeader = () => {
     const [openStakeModul, setOpenStaekeModul] = useState<boolean>(false);
     const [isDisableStakeButton, setIsDisableStakeButton] = useState(true);
+    const { accountId } = useAuroWalletCore();
+    const { balance } = useAddressBalance(accountId?.[0] ?? null);
 
     return (
         <div className={style.wrapper}>
+            <span className={classNames(style.balance, 't-inter-medium')}>
+                Balance:
+                <Image src={DollarIcon} alt="" /> {balance}
+            </span>
             <ConnectWalletButton handleAddress={setIsDisableStakeButton} />
             <Button
                 className={style.stakeButton}
